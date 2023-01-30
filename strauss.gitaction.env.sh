417168459e292320a68d1dae8488037a6458dc32
squidpie <squidpie_meowface@pm.me>
2023-01-29 23:44:25 -0800

    More tweaks

--- strauss.gitaction.env.sh
+++ strauss.gitaction.env.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export STRAUSS_ROOT_DIR=$(pwd)
 export STRAUSS_BUILD_DIR=${STRAUSS_ROOT_DIR}/target/debug
 #export GitVersion_SemVer=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo | \
