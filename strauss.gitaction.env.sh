09a2ce5ceb7d4c8a730b2e8a5e4d18f9fc4b3768
squidpie <squidpie_meowface@pm.me>
2023-01-29 23:36:25 -0800

    Tweaks gitaction env

--- strauss.gitaction.env.sh
+++ strauss.gitaction.env.sh
@@ -1,4 +1,5 @@
 export STRAUSS_ROOT_DIR=$(pwd)
 export STRAUSS_BUILD_DIR=${STRAUSS_ROOT_DIR}/target/debug
-export GitVersion_SemVer=$(sudo docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo | \
-  python3 -c "import sys, json; print(json.load(sys.stdin)['SemVer'])")
+#export GitVersion_SemVer=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo | \
+#  python3 -c "import sys, json; print(json.load(sys.stdin)['SemVer'])")
+export GitVersion_SemVer=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo)
