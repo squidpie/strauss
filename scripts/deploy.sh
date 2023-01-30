0ebd37ce33761de4841e46c4887d27baf55721c4
squidpie <squidpie_meowface@pm.me>
2023-01-30 07:00:08 -0800

    Changes deploy script to fail if a step fails

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 # Copyright (C) 2023
 # Squidpie
 
-docker compose -f docker-compose.yml -f docker-compose.prod.yml pull
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --remove-orphans
+docker compose -f docker-compose.yml -f docker-compose.prod.yml pull && \
+docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --remove-orphans && \
 docker image prune -f
