12dd0691592e432d217f67c632b0407e41a40c08
Squidpie Meowface <squidpie_meowface@pm.me>
2023-01-31 23:00:49 -0800

    Rework env setup

---
+++ scripts/gen-dev-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# gen-dev-env.sh
+# Copyright (C) 2023
+# Squidpie
+echo \
+"STRAUSS_ROOT_DIR=$(pwd)
+STRAUSS_BUILD_DIR=$(pwd)/target/debug
+GITVERSION_SEMVER=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo | \
+  python3 -c "import sys, json; print(json.load(sys.stdin)['SemVer'])")" > .env
+echo `services/chat/env.rb` >> .env
