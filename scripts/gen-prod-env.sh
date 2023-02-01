bc295867084e66ac51f394432523fcace9ac8435
squidpie <squidpie_meowface@pm.me>
2023-01-31 22:25:09 -0800

    Tweaks prod env

--- scripts/gen-prod-env.sh
+++ scripts/gen-prod-env.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-echo \
-"STRAUSS_ROOT_DIR=$(pwd)
-STRAUSS_BUILD_DIR=$(pwd)/target/release
-GITVERSION_SEMVER=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.6 /repo | \
-  python3 -c "import sys, json; print(json.load(sys.stdin)['SemVer'])")" > .env
 echo `services/chat/env.rb` >> .env
