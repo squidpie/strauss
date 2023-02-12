0199a9479d0431f7c9e25de104be9cc5935e491d
squidpie <squidpie_meowface@pm.me>
2023-02-12 04:50:21 -0800

    ci: this is strange. copied redis test

--- tests/debug/run.rb
+++ tests/debug/run.rb
@@ -6,41 +6,40 @@
 require 'redis'
 require 'json'
 
-$rx = "#strauss-chat-msg-rx";
-$tx = "#strauss-chat-msg-tx";
-$test_msg="meow meow meow";
+$ch = "#strauss-chat-test";
+$test_msg="0xdeadbeef"
 
 redis = Redis.new(url: "redis://localhost:6379")
 
 def client(redis)
-    begin
-        redis.subscribe($rx) do |on|
-            on.subscribe do |channel, subscriptions|
-                puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)"
-            end
+  begin
+    redis.subscribe($ch) do |on|
+      on.subscribe do |channel, subscriptions|
+        puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)"
+      end
 
-on.message do |channel, msg|
-    puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]"
-    if msg != $test_msg
+    on.message do |channel, msg|
+      puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]"
+      if msg != $test_msg
         puts "::TEST OUTPUT:: REDIS RECEIVE TEST FAILED - #{msg}"
         exit(-1)
+      end
+      exit(0)
     end
-    exit(0)
-end
-end
+  end
 rescue=>error
-    puts "::TEST OUTPUT:: REDIS SUBSCRIBE TEST FAILED - #{error}"
-    exit(-1)
+  puts "::TEST OUTPUT:: REDIS SUBSCRIBE TEST FAILED - #{error}"
+  exit(-1)
 end
 end
 
 def publisher(redis)
-    begin
-        redis.publish($tx, $test_msg)
-    rescue=>error
-        puts "::TEST OUTPUT:: REDIS PUBLISH TEST FAILED - #{error}"
-        exit(-1)
-    end
+  begin
+    redis.publish($ch, $test_msg)
+  rescue=>error
+    puts "::TEST OUTPUT:: REDIS PUBLISH TEST FAILED - #{error}"
+    exit(-1)
+  end
 end
 
 client = Thread.new { client(redis) }
@@ -48,3 +47,4 @@ sleep(0.1)
 publisher = Thread.new { publisher(redis) }
 publisher.join()
 client.join()
+
