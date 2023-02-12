dedc97b495a2825c22f9ee53f59b14c18f8efe6d
squidpie <squidpie_meowface@pm.me>
2023-02-12 04:45:01 -0800

    ci: copy content from redis test

--- tests/debug/run.rb
+++ tests/debug/run.rb
@@ -14,34 +14,31 @@ redis = Redis.new(url: "redis://localhost:6379")
 
 def client(redis)
     begin
-        puts "::TEST OUTPUT:: Starting Chat Integration Test";
         redis.subscribe($rx) do |on|
             on.subscribe do |channel, subscriptions|
-                puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)";
+                puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)"
             end
 
 on.message do |channel, msg|
-    msg = JSON.parse(msg)['message_text']
-    puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]";
+    puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]"
     if msg != $test_msg
-        puts "::TEST OUTPUT:: CHAT TEST FAILED - #{msg}";
+        puts "::TEST OUTPUT:: REDIS RECEIVE TEST FAILED - #{msg}"
         exit(-1)
     end
     exit(0)
 end
 end
 rescue=>error
-    puts "::TEST OUTPUT:: REDIS SUBSCRIBE FAILED - #{error}";
+    puts "::TEST OUTPUT:: REDIS SUBSCRIBE TEST FAILED - #{error}"
     exit(-1)
 end
 end
 
 def publisher(redis)
     begin
-        puts "::TEST OUTPUT:: Sending Test Chat Message";
         redis.publish($tx, $test_msg)
     rescue=>error
-        puts "::TEST OUTPUT:: REDIS PUBLISH FAILED - #{error}";
+        puts "::TEST OUTPUT:: REDIS PUBLISH TEST FAILED - #{error}"
         exit(-1)
     end
 end
