b45330e65943c9bb5b7b7fdfb8efe501c4bee796
squidpie <squidpie_meowface@pm.me>
2023-02-12 02:55:19 -0800

    ci: Try to reformat chats run.rb

--- tests/chat/run.rb
+++ tests/chat/run.rb
@@ -8,44 +8,45 @@ require 'json'
 
 $rx = "#strauss-chat-msg-rx";
 $tx = "#strauss-chat-msg-tx";
-$test_msg="meow meow meow"
+$test_msg="meow meow meow";
 
 redis = Redis.new(url: "redis://redis:6379")
 
 def client(redis)
-  begin
-    puts "::TEST OUTPUT:: Starting Integration Test For Chat"  
-    redis.subscribe($rx) do |on|
-      on.subscribe do |channel, subscriptions|
-          puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)"
-      end
+    begin
+        puts "::TEST OUTPUT:: Starting Integration Test For Chat";
+        redis.subscribe($rx) do |on|
+            on.subscribe do |channel, subscriptions|
+                puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)";
+            end
 
-      on.message do |channel, msg|
-        msg = JSON.parse(msg)['message_text']
-        puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]"
-        if msg != $test_msg
-          puts "::TEST OUTPUT:: REDIS RECEIVE TEST FAILED - #{msg}"
-          exit(-1)
+            on.message do |channel, msg|
+                msg = JSON.parse(msg)['message_text']
+                puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]";
+                if msg != $test_msg
+                    puts "::TEST OUTPUT:: CHAT TEST FAILED - #{msg}";
+                    exit(-1)
+                end
+                exit(0)
+            end
         end
-        exit(0)
-      end
+    rescue=>error
+        puts "::TEST OUTPUT:: REDIS SUBSCRIBE FAILED - #{error}";
+        exit(-1)
     end
-  rescue=>error
-    puts "::TEST OUTPUT:: REDIS SUBSCRIBE TEST FAILED - #{error}"
-    exit(-1)
-  end
 end
 
 def publisher(redis)
-  begin
-    puts "::TEST OUTPUT:: Sending Test Chat Message"
-    redis.publish($tx, $test_msg)
-  rescue=>error
-    puts "::TEST OUTPUT:: REDIS PUBLISH TEST FAILED - #{error}"
-    exit(-1)
-  end
+    begin
+        puts "::TEST OUTPUT:: Sending Test Chat Message";
+        redis.publish($tx, $test_msg)
+    rescue=>error
+        puts "::TEST OUTPUT:: REDIS PUBLISH FAILED - #{error}";
+        exit(-1)
+    end
 end
 
+puts "::TEST OUTPUT:: STARTING CHAT TEST";
 client = Thread.new { client(redis) }
 sleep(0.1)
 publisher = Thread.new { publisher(redis) }
