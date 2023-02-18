1abd0baf9d2990f0ff1d0e2f36324f6e3244387e
squidpie <squidpie_meowface@pm.me>
2023-02-18 11:02:02 -0800

    ci: Adds test flush and && to command

--- tests/redis/run.rb
+++ tests/redis/run.rb
@@ -16,12 +16,14 @@ def client(redis)
     redis.subscribe($ch) do |on|
       on.subscribe do |channel, subscriptions|
         puts "::TEST OUTPUT:: Subscribed to #{channel} (#{subscriptions} subscriptions)"
+        STDOUT.flush
       end
 
     on.message do |channel, msg|
       puts "::TEST OUTPUT:: Received #{channel} - [#{msg}]"
       if msg != $test_msg
         puts "::TEST OUTPUT:: REDIS RECEIVE TEST FAILED - #{msg}"
+        STDOUT.flush
         exit(-1)
       end
       exit(0)
@@ -29,6 +31,7 @@ def client(redis)
   end
 rescue=>error
   puts "::TEST OUTPUT:: REDIS SUBSCRIBE TEST FAILED - #{error}"
+  STDOUT.flush
   exit(-1)
 end
 end
@@ -38,6 +41,7 @@ def publisher(redis)
     redis.publish($ch, $test_msg)
   rescue=>error
     puts "::TEST OUTPUT:: REDIS PUBLISH TEST FAILED - #{error}"
+    STDOUT.flush
     exit(-1)
   end
 end
