a947b69e4836a547ab02cf7664724205fac2f5df
squidpie <squidpie_meowface@pm.me>
2023-02-18 16:50:20 -0800

    ci: All in one big compose project

--- tests/redis/run.rb
+++ tests/redis/run.rb
@@ -9,7 +9,7 @@ require 'json'
 $ch = "#strauss-chat-test";
 $test_msg="0xdeadbeef"
 
-redis = Redis.new(url: "redis://localhost:6379")
+redis = Redis.new(url: "redis://redis:6379")
 
 def client(redis)
   begin
@@ -26,6 +26,8 @@ def client(redis)
         STDOUT.flush
         exit(-1)
       end
+      puts "*** PASSED ***"
+      STDOUT.flush
       exit(0)
     end
   end
